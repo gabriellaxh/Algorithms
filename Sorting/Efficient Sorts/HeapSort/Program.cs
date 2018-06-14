af6a1f587bbc98b2034327161069b0b5113a1be8
Gabriella Shirkova <gabriellashirkova@gmail.com>
2018-06-14 11:29:10 +0300

    Exercises

---
+++ Sorting/Efficient Sorts/HeapSort/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace HeapSort
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = new int[] { 63, 17, 10, 16, 3, 38 };
+
+            Sort(arr);
+
+            Console.WriteLine(string.Join(' ', arr));
+        }
+
+        private static void Sort(int[] array)
+        {
+            int n = array.Length;
+
+            // Build heap (rearrange array)
+            for (int i = n / 2 - 1; i >= 0; i--)
+            {
+                HeapSort(array, n, i);
+            }
+
+            for (int i = n-1; i >= 0; i--)
+            {
+                // Move current root to end(the biggest num to the end)
+                int temp = array[i];
+                array[i] = array[0];
+                array[0] = temp;
+
+                HeapSort(array, i, 0);
+            }
+        }
+
+        private static void HeapSort(int[] array, int n, int i)
+        {
+            int largest = i;
+            int leftChild = 2 * i + 1;
+            int rightChild = 2 * i + 2;
+
+            if (leftChild < n && array[leftChild] > array[largest])
+                largest = leftChild;
+
+            if (rightChild < n && array[rightChild] > array[largest])
+                largest = rightChild;
+
+            if (largest != i)
+            {
+                int temp = array[i];
+                array[i] = array[largest];
+                array[largest] = temp;
+
+                // Recursively heapify the affected sub-tree
+                HeapSort(array, n, largest);
+            }
+        }
+    }
+}
---
+++ Sorting/Efficient Sorts/QuickSort/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace QuickSort
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = new int[] { 6, 5, 3, 0, -20, 1, 8, 7, 2, 4 };
+
+            Sort(arr, 0, arr.Length - 1);
+
+            Console.WriteLine(string.Join(' ', arr));
+        }
+
+        private static void Sort(int[] array, int left, int right)
+        {
+            if (left < right)
+            {
+                int pivot = QuickSort(array, left, right);
+
+                Sort(array, left, pivot - 1);
+
+                Sort(array, pivot + 1, right);
+            }
+        }
+
+        private static int QuickSort(int[] array, int left, int right)
+        {
+            int temp;
+
+            int pivot = array[right];
+
+            int idxLastSmallerThanPivotNum = left - 1;
+
+            for (int j = left; j < right; j++)
+            {
+                if (array[j] <= pivot)
+                {
+                    idxLastSmallerThanPivotNum++;
+
+                    temp = array[idxLastSmallerThanPivotNum];
+                    array[idxLastSmallerThanPivotNum] = array[j];
+                    array[j] = temp;
+                }
+            }
+
+            //moves the pivot on the right place in the list
+            temp = array[idxLastSmallerThanPivotNum + 1];
+            array[idxLastSmallerThanPivotNum + 1] = array[right];
+            array[right] = temp;
+
+            return idxLastSmallerThanPivotNum + 1;
+        }
+    }
+}
