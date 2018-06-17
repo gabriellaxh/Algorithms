aa07e3be63f5f63aa9357f5d0bcd3bc36231df1a
Gabriella Shirkova <gabriellashirkova@gmail.com>
2018-06-17 13:17:24 +0300

    Exercises

--- Searching/LinearSearch/Program.cs
+++ Searching/LinearSearch/Program.cs
@@ -6,7 +6,22 @@ namespace LinearSearch
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] array = new int[] { 4, 5, 7, 754, 23, 52, 9, 2, 1 };
+
+            Console.WriteLine(LinearSearch(array,-1));
+        }
+
+        public static bool LinearSearch(int[] array, int item)
+        {
+            int n = array.Length;
+
+            for (int i = 0; i < n; i++)
+            {
+                if (array[i] == item)
+                    return true;               
+            }
+
+            return false;
         }
     }
 }
