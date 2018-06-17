c77d8a1f18880aeebacc59b779e664b2e18774f3
Gabriella Shirkova <gabriellashirkova@gmail.com>
2018-06-17 13:29:23 +0300

    Exercises

--- Searching/BinarySearch/Program.cs
+++ Searching/BinarySearch/Program.cs
@@ -2,11 +2,31 @@
 
 namespace BinarySearch
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] array = new int[] { 4, 5, 7, 9, 23, 52, 69, 72, 91 };
+
+            Console.WriteLine(BinarySearch(array,0,array.Length-1, 69));
+        }
+
+        public static bool BinarySearch(int[] array, int start, int end, int item)
+        {
+            if (start < end)
+            {
+                int middle = (start + end) / 2;
+
+                if (array[middle] == item)
+                    return true;
+
+                else if (array[middle] > item)
+                    return BinarySearch(array, start, middle - 1, item);
+
+                else
+                    return BinarySearch(array, middle + 1, end, item);
+            }
+            return false;
         }
     }
 }
